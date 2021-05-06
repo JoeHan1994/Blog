0acdfb2448f5ec3c281216faa5b676101762b5e2
JoeHan1994 <71834618+JoeHan1994@users.noreply.github.com>
2021-05-05 19:03:56 -0700

    Add files via upload

---
+++ Blog.IRepository/IBaseRepository.cs
@@ -0,0 +1,48 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Blog.IRepository
+{
+    public interface IBaseRepository<TEntity> where TEntity:class,new()
+    {
+        Task<bool> CreateAsync(TEntity entity);
+        Task<bool> DeleteAsync(int Id);
+        Task<bool> EditAsync(TEntity entity);
+        Task<TEntity> FindAsync(int Id);
+        Task<TEntity> FindAsync(Expression<Func<TEntity, bool>> func);
+        /// <summary>
+        /// Get all data
+        /// </summary>
+        /// <returns></returns>
+        Task<List<TEntity>> QueryAsync();
+        /// <summary>
+        /// custome condition query
+        /// </summary>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        Task<List<TEntity>> QueryAsync(Expression<Func<TEntity,bool>> func);
+        /// <summary>
+        /// split page query
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="size"></param>
+        /// <param name="total"></param>
+        /// <returns></returns>
+        Task<List<TEntity>> QueryAsync(int page, int size, RefAsync<int> total);
+        /// <summary>
+        /// custome split page query
+        /// </summary>
+        /// <param name="func"></param>
+        /// <param name="page"></param>
+        /// <param name="size"></param>
+        /// <param name="total"></param>
+        /// <returns></returns>
+        Task<List<TEntity>> QueryAsync(Expression<Func<TEntity, bool>> func, int page, int size, RefAsync<int> total);
+
+
+
+    }
+}
